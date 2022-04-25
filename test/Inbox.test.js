21dd86e33b63d7e1eeae26efeabd11eb0ad66b6a
Eyal <eyalpross101@gmail.com>
2022-04-25 21:17:12 +0300

    add tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,18 +8,33 @@ const { interface, bytecode } = require("../compile");
 let accounts;
 let inbox;
 
+const INITIAL_STRING ="Hi there"
+
 beforeEach(async () => {
   //Get  a list of all accounts
   accounts = await web3.eth.getAccounts();
   //use one of those account to deploy the contract
 
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    assert.ok(inbox.options.address)
+    assert.ok(inbox.options.address);
   });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message,[INITIAL_STRING])
+  });
+
+  it("can change the message", async () => {
+    const newMessage = "bye"
+    await inbox.methods.setMessage(newMessage).send({from: accounts[0]}); //return hash
+    const returnMessage = await inbox.methods.message().call();
+    assert.equal(newMessage,returnMessage)
+  });
+
 });
