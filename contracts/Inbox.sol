f64ca478ffa284ac3e69e744192e976f52d06984
Eyal <eyalpross101@gmail.com>
2022-04-25 18:41:49 +0300

    base

---
+++ contracts/Inbox.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.17;
+// linter warnings (red underline) about pragma version can igonored!
+
+// contract code will go here
+
+contract Inbox {
+    string public message;
+    
+    function Inbox (string initialMeessage) public {
+        message = initialMeessage;
+    }
+
+    function setMessage (string newMessage) public {
+        message = newMessage;
+    }
+ 
+
+}
